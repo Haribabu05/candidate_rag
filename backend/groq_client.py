0343dd83f1119cdc70da5113798eeb030ba3ca24
Haribabu <sharibabu507@gmail.com>
2026-08-26 03:40:58 +0530

    new model in groq_client.py

--- backend/groq_client.py
+++ backend/groq_client.py
@@ -20,8 +20,7 @@ def ask_gemini(prompt):
 
     response = client.chat.completions.create(
 
-        model="llama-3.1-8b-instant",  
-
+        model="openai/gpt-oss-20b",  
         messages=[
             {
                 "role": "user",
